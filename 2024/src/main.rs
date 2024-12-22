ffad9beacc7cb7af9db8bf7d7f1ed44e7f8e20b0
V1OL3TF0X <mattex15243@gmail.com>
2024-12-22 17:34:00 +0100

    Day 22 both tasks

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -3,7 +3,7 @@ use std::array::IntoIter;
 use aoc_2024::utils::{get_input, get_sample_input, measure_elapsed};
 use clap::{Parser, ValueEnum};
 
-const AOC_PROBLEM_NO: usize = 20;
+const AOC_PROBLEM_NO: usize = 22;
 
 fn main() {
     let args = Args::parse();
@@ -162,6 +162,14 @@ fn get_all_solution_fns() -> IntoIter<TaskFns, AOC_PROBLEM_NO> {
         TaskFns {
             task_1: Box::new(aoc_2024::day_20::task_1),
             task_2: Box::new(aoc_2024::day_20::task_2),
+        },
+        TaskFns {
+            task_1: Box::new(aoc_2024::day_21::task_1),
+            task_2: Box::new(aoc_2024::day_21::task_2),
+        },
+        TaskFns {
+            task_1: Box::new(aoc_2024::day_22::task_1),
+            task_2: Box::new(aoc_2024::day_22::task_2),
         },
     ]
     .into_iter()
